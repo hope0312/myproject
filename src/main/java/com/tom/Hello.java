e6a4b1c06e3a82238831e4c7f97a00346fc9826d
Hope <hope031272@gmail.com>
2020-08-07 11:59:19 +0800

    Jave and kotlin varrible, date type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -1,10 +1,21 @@
 package com.tom;
 
+import java.sql.SQLOutput;
+
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world!\n");
 //        new Person().hello();
-        Person p = new Person();
-        p.hello();
+//        Person p = new Person();
+//        p.hello();
+        int age = 15;
+        double weight = 66.5;
+        float hight = 66.5f;
+        char h ='h';
+        String name = "Tom";
+        byte b = 120;
+        boolean enroll = true;
+        String original;
+        System.out.println(enroll);
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -3,8 +3,14 @@ package com.tom
 fun main(args: Array<String>) {
 //    println("Hello kotlin!!\n");
 //    Human().hello();
-    val h = Human();
-    h.hello();
+//    val h = Human();
+//    h.hello();
+    var age : Int = 9;
+    age = 20;
+    var weight = 66.5;
+    var name :String;
+    name = "Hank;"
+    var cantoll : Boolean = true;
 }
 
 class Human {
